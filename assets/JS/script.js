6f67a0a2eab5b22844dd833a42b9da4343d31e42
oscking <osckinglh@gmail.com>
2023-02-11 22:00:31 -0600

    Version 1
    Pagina de Nuv este

---
+++ assets/JS/script.js
@@ -0,0 +1,17 @@
+var scrollBtn = document.getElementById("scrollBtn");
+if (scrollBtn) {
+  scrollBtn.addEventListener("click", function() {
+    window.scroll({
+      top: 500, 
+      left: 0, 
+      behavior: 'smooth'
+    });
+  });
+}
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+menu.classList.toggle('active');
+});
